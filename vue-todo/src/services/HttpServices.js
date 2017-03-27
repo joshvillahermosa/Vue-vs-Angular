a4fc86c9f99c7a0d070c5e04add2558ad1c5fb78
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-03-27 16:44:07 -0400

    feat(http-service): Added general HTTP Service

---
+++ vue-todo/src/services/HttpServices.js
@@ -0,0 +1,19 @@
+/**
+ * Vue Http Services
+ */
+import Vue from 'vue';
+
+import * as VueResource from 'vue-resource';
+
+export default class TodoHttpService {
+  constructor() {
+    Vue.use(VueResource);
+    this.http = Vue.http;
+    this.root = 'http://localhost:3100/api/';
+  }
+
+  get = (resource) => {
+    const promise = this.http.get(`${this.root}${resource}`);
+    return promise;
+  }
+}
