8562da65499b72becc6338b10cc11fb2e64d9e6e
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-03-27 16:46:01 -0400

    refactor(router): Update router names

--- vue-todo/src/router/index.js
+++ vue-todo/src/router/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import TodoView from '@/components/TodoView';
-// import TodoHttpService from '../services/HttpServices';
-
-// const todoTest = new TodoHttpService();
-
-// todoTest.get('todo').then(data => console.log(data.body));
 
 Vue.use(Router);
 
