289f6200e98cd5ca9435210001d24d3732dc14de
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-03-27 16:42:17 -0400

    fix(todo-api): Allow Cors API

--- todo-api/app.js
+++ todo-api/app.js
@@ -9,17 +9,29 @@ const todoDb    = new JsonDb('dbs/todo');
 const port      = process.env.PORT || 3100;
 
 /**
+ * Allows CORS atm.
+ */
+function allowCors (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
+
+/**
  * Route Handlers. Kept in file for simplicity (I know this should be better
  * but w.e.)
  */
-const todoHandler = (req, res) => {
+function todoHandler (req, res) {
   const todoData = todoDb.getData('/todo');
   res.status(200).json(todoData);
-}
+};
+
 
 /**
  * Start server
  */
+app.use(allowCors);
 router.get('/api/todo', todoHandler);
 app.use(router);
 app.listen(port);
