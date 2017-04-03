5df305e081b865cc9bfa28f276a7c26b026d8525
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-04-03 09:57:55 -0400

    feat(todo-item-component): WIP on creating a update/delete functionality

--- vue-todo/src/components/shared/TodoItem.vue
+++ vue-todo/src/components/shared/TodoItem.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-    <input type="checkbox" v-model="todo.completed"> 
-    <span :class="{'strike-through': todo.completed}">{{ todo.todo }}</span>
+    <input
+      type="checkbox"
+      v-model="todo.completed"> 
+
+    <span v-if="!editingItem">
+      <span :class="{'strike-through': todo.completed}">{{ todo.todo }}</span>
+
+      <a 
+        href="#"
+        v-on:click="toggleEdit"> Edit </a>
+    </span>
+
+    <span v-if="editingItem">
+      <input
+        type="input"
+        v-model="todo.todo">
+
+      <a 
+        href="#"
+        v-on:click="toggleEdit"> Save </a>
+
+      <a 
+        href="#"
+        v-on:click="toggleEdit"> Cancel </a>
+    </span>
+
     <a href="#" v-on:click="deleteItem"> Delete </a>
   </div>
 </template>
@@ -10,11 +34,21 @@
   export default {
     name: 'TodoItem',
     props: {
-      todo: Object,
+      todo: {
+        type: Object,
+        default: {},
+      },
+    },
+    data() {
+      return { editingItem: false };
     },
     methods: {
       deleteItem() {
-        alert('Item will soon be deleted');
+        // alert('Item will soon be deleted');
+      },
+      toggleEdit() {
+        // @todo Added a way to keep state change
+        this.editingItem = !this.editingItem;
       },
     },
   };
